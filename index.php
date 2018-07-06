b19e03eb26bbbd5132f6e4aa64c69f2ba2696e2a
simaandsima <simaandsima@gmail.com>
2018-07-06 23:36:31 +0300

    xx

--- index.php
+++ index.php
@@ -11,26 +11,5 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
-
-<<<<<<< HEAD
 //git com
-dcncndcs
-
-do {
-    # code...
-} while (cairo_ps_surface_dsc_comment(cairo_ps_surface_dsc_comment(cairo_ps_surface_dsc_comment(
-
-    hw_Document_SetContent(
-
-
-        dcs
-
-
-
-        cairo_surface_create_similar(dcsscd
-            scs
-            <dd>cairo_surface_create_similar(dcsscddsc, content, width, height)</dd>, content, width, height), content), comment), comment), comment));
-=======
-//git com test subl
->>>>>>> refs/remotes/origin/origin
-
+xxxxx
